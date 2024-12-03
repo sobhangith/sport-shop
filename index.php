11f449dba0986f050825b9268cd457084eb61e5d
sobhangith <sobhangith@gmail.com>
2024-12-03 13:35:51 +0330

    v.2

--- index.php
+++ index.php
@@ -1,7 +1,159 @@
 <?php include("pageheader.html"); ?>
 
 <div class="main-content">
-    <div id="welcome-text">
-        <p>به فروشگاه لوازم ورزشی خوش آمدید!</p>
-    </div>
+<section>
+<h2 id="populer">محصولات پر فروش</h2>
+    <section class="products">
+        <div class="product">
+            <div>
+            <img src="images/image_bas.jpg" alt="توپ بسکتبال">
+            </div>
+            <h3>توپ بسکتبال</h3>
+            <p>قیمت: 300,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="توپ بسکتبال">
+                <input type="hidden" name="productPrice" value="300000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+        <div class="product">
+            <img src="images/top.jpg" alt="توپ فوتبال">
+            <h3>توپ فوتبال</h3>
+            <p>قیمت: 250,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="توپ فوتبال">
+                <input type="hidden" name="productPrice" value="250000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+        <div class="product">
+            <img src="images/kafsh.jpg" alt="کفش فوتبال">
+            <h3>کفش فوتبال</h3>
+            <p>قیمت: 400,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="کفش فوتبال">
+                <input type="hidden" name="productPrice" value="400000">
+                <button type="submit">اضافه به سبد خرید</button>
+                </div>
+            </form>
+            <div class="product">
+            <img src="images/Acmilan.webp" alt="کیت آث میلان">
+            <h3>کیت آث میلان</h3>
+            <p>قیمت: 700,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="کیت آث میلان">
+                <input type="hidden" name="productPrice" value="700000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/barsa.jpg" alt="کیت بارسلونا">
+            <h3>کیت بارسلونا</h3>
+            <p>قیمت: 680,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="کیت بارسلونا">
+                <input type="hidden" name="productPrice" value="680000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/dambel.jpg" alt="دمبل 10 کیلویی">
+            <h3>دمبل ده کیلویی</h3>
+            <p>قیمت: 250,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="دمبل ده کیلویی">
+                <input type="hidden" name="productPrice" value="250000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/dast.jpg" alt="دستکش دروازه بان">
+            <h3>دستکش دروازه بان</h3>
+            <p>قیمت: 999,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="دستکش دروازه بان">
+                <input type="hidden" name="productPrice" value="700000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>     
+        
+        
+    </section>
+    <div> <h3 id="takhfif">محصولات تخفیف خورده</h3></div>
+    <div class="main-content">
+<section>
+    <section class="products">
+        <div class="product">
+            <div>
+            <img src="images/bace.webp" alt="توپ بیسبال">
+            </div>
+            <h3>توپ بیسبال</h3>
+            <p>قیمت: 350,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="توپ بیسبال">
+                <input type="hidden" name="productPrice" value="300000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+        <div class="product">
+            <img src="images/rocket.jpg" alt="راکت بدمینتون">
+            <h3>راکت بدمینتون</h3>
+            <p>قیمت: 270,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="راکت بدمینتون">
+                <input type="hidden" name="productPrice" value="250000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+        <div class="product">
+            <img src="images/shalvar.jpg" alt="شلوار ورزشی">
+            <h3>شلوار ورزشی</h3>
+            <p>قیمت: 500,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="شلوار ورزشی">
+                <input type="hidden" name="productPrice" value="400000">
+                <button type="submit">اضافه به سبد خرید</button>
+                </div>
+            </form>
+            <div class="product">
+            <img src="images/tshirt.jpg" alt="تیشرت ورزشی">
+            <h3>تیشرت ورزشی</h3>
+            <p>قیمت: 330,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="تیشرت ورزشی">
+                <input type="hidden" name="productPrice" value="700000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/tanab.webp" alt="طناب ورزشی">
+            <h3>طناب ورزشی</h3>
+            <p>قیمت: 55,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value=" طناب ورزشی">
+                <input type="hidden" name="productPrice" value="55000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/dambel50.webp" alt="دمبل 50 کیلویی">
+            <h3>دمبل پنجاه کیلویی</h3>
+            <p>قیمت: 400,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="دمبل پنجاه کیلویی">
+                <input type="hidden" name="productPrice" value="250000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>
+            <div class="product">
+            <img src="images/kafsh1.jpg" alt="کفش پیاده روی">
+            <h3>کفش پیاده روی</h3>
+            <p>قیمت: 800,000 تومان</p>
+            <form method="POST">
+                <input type="hidden" name="productName" value="کفش پیاده روی">
+                <input type="hidden" name="productPrice" value="800000">
+                <button type="submit">اضافه به سبد خرید</button>
+            </form>
+        </div>     
+</div>
 <?php include("pagefooter.html"); ?>
