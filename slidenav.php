e37396c0b7db9971b433632921638569ffe8c328
sobhan <you@gmail.com>
2024-12-24 12:17:49 +0330

    v3

---
+++ slidenav.php
@@ -0,0 +1,13 @@
+<?php include("pageheader.html"); ?>
+<?php
+$txt=$_POST["txt"];
+$email=$_POST["email"];
+$Phone=$_POST["Phone"];
+$pswd=$_POST["pswd"];
+echo("<p> .$txt.</p> ");
+echo("<p> .$email.</p> ");
+echo("<p> .$Phone.</p> ");
+echo("<p> .$pswd.</p> ");
+
+?>
+<?php include("pagefooter.html"); ?>
---
+++ slidenav1.php
@@ -0,0 +1,8 @@
+<?php include("pageheader.html"); ?>
+<?php
+$email=$_POST["email"];
+$pswd=$_POST["pswd"];
+echo("<p> .$email.</p> ");
+echo("<p> .$pswd.</p> ");
+?>
+<?php include("pagefooter.html"); ?>
