716ea669be7a6104141ef98c7f1b5ff37bc6dd37
sobhangith <sobhangith@gmail.com>
2025-02-04 13:42:17 +0330

    add db

---
+++ site.sql
@@ -0,0 +1,69 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Feb 04, 2025 at 10:09 AM
+-- Server version: 8.3.0
+-- PHP Version: 8.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `site`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+DROP TABLE IF EXISTS `login`;
+CREATE TABLE IF NOT EXISTS `login` (
+  `email` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `pswd` varchar(30) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signin`
+--
+
+DROP TABLE IF EXISTS `signin`;
+CREATE TABLE IF NOT EXISTS `signin` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `txt` varchar(30) NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `phone` varchar(30) NOT NULL,
+  `pswd` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `signin`
+--
+
+INSERT INTO `signin` (`id`, `txt`, `email`, `phone`, `pswd`) VALUES
+(1, '[ssd]', '[sbhanrx@gmail.com]', '[0901]', '[sss]'),
+(2, '[sobhan]', '[alkfassf@gmail.com]', '[123]', '[as12qw]'),
+(3, '[]', '[]', '[]', '[]'),
+(4, '[sobhan]', '[sbhanrx@gmail.com]', '[123]', '[aaa]'),
+(5, '[sobhan]', '[sbhanrx@gmail.com]', '[123]', '[asa]'),
+(6, 'sobhan', 'sbhanrx@gmail.com', '09123', 'sm123'),
+(7, 'aa', 'sm@gmail.com', '123', 'sm12'),
+(8, 'amir', 'a@G.C', '1213123', '1234');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
